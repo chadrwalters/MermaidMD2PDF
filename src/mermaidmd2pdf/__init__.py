ed53bfde77b205e314eb5bcdf1521524f70ca56a
Chad Walters <chad.walters@gmail.com>
2025-03-08 10:20:56 -0600

    fix: update package configuration for proper src layout handling with uv

--- src/mermaidmd2pdf/__init__.py
+++ src/mermaidmd2pdf/__init__.py
@@ -1,8 +1,8 @@
 """MermaidMD2PDF package."""
 
-from mermaidmd2pdf.generator import ImageGenerator, PDFGenerator
-from mermaidmd2pdf.processor import MermaidProcessor
-from mermaidmd2pdf.validator import FileValidator
+from .generator import ImageGenerator, PDFGenerator
+from .processor import MermaidProcessor
+from .validator import FileValidator
 
 __version__ = "0.1.0"
 
